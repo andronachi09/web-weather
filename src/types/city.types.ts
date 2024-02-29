7c25eaa3e0d310aaed6433080a0c62617d36fc92
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2024-02-29 22:48:05 +0100

    chore: city type

---
+++ src/types/city.types.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export type CityType = {
+    cityName: string,
+    country: string,
+    state: string,
+    lat: number,
+    lon: number,
+    children?: ReactNode;
+};
