11ad0d54104dbfcf6b2f8fb0c0ba84b3d9aa9c31
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2024-03-08 19:55:44 +0100

    chore: removing comments App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import SearchWithResults from "./components/v-01/organisms/SearchWithResults";
+import MainView from './components/v-02/templates/MainView';
 
 export default function App() {
-  return (
-      <div className="flex h-screen w-screen items-center justify-center bg-[#101720]">
-        <SearchWithResults
-          className="flex flex-col items-center py-16"
-        />
-      </div>
-  );
+	return (
+		<div>
+			<MainView />
+		</div>
+	);
 }
-
