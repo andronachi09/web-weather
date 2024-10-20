18904f019b01a7bd420432277dd8648feda50b68
andronachi09 <tudor.andronachi09@gmail.com>
2024-10-20 17:15:37 +0200

    chore: test commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainView from './components/v-02/pages/MainView';
 
 export default function App() {
 	const apiKey: string = import.meta.env.VITE_WEATHER_API_KEY;
+	const test: string = ''
 	return (
 		<>
 			<SearchProvider apiKey={apiKey}>
